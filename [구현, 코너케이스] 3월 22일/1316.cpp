53693782dcdaa7e4f0d21bc623e40f25501dccf6
JeonJaeeun <jane9518@naver.com>
2022-03-29 01:29:24 +0900

    [구현, 코너케이스] 3월 22일
    1316번 과제 수정해서 제출합니다.

--- [구현, 코너케이스] 3월 22일/1316.cpp
+++ [구현, 코너케이스] 3월 22일/1316.cpp
@@ -15,15 +15,15 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> word; // 단어
 		bool check = true;
+		int alpha[26] = { 0, };
 
 		// 입력받은 단어의 길이만큼 for문
 		for (int j = 0; j < word.size(); j++) {
-			for (int k = 0; k < j; k++) {
-				// j번째 단어와 j-1번째 단어가 다르면 false
-				// 앞에 나왔던 단어가 또 나오면 false
-				if (word[j] != word[j - 1] && word[j] == word[k]) {
+			alpha[(int)word[j] - 97]++;
+			// j번째 단어와 j-1번째 단어가 다르면 false
+			// 앞에 나왔던 단어가 또 나오면 false
+			if (alpha[(int)word[j] - 97] > 1 && word[j] != word[j - 1]) {
 					check = false;
-				}
 			}
 		}
 		if (check)
